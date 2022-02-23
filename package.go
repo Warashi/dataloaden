5dabbcd28847aa1a5aa0dbcfa932c544eeee9cc1
Shinnosuke Sawada <6warashi9@gmail.com>
2022-02-23 12:11:34 +0900

    docs: add package document

---
+++ package.go
@@ -0,0 +1,5 @@
+// github.com/Warashi/dataloaden is the dataloader pattern library for a GraphQL server.
+// this library is built based on github.com/vektah/dataloaden.
+// code generated with vektah/dataloaden then added type parameters.
+// this library is using type parameters, so needs Go 1.18 or later.
+package dataloaden
