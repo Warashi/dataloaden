8ca8bb85c38913092b890186f243fa0c7afb5fe0
Shinnosuke Sawada <6warashi9@gmail.com>
2022-03-03 06:42:14 +0900

    test: add tests

---
+++ loader_test.go
@@ -0,0 +1,97 @@
+package dataloaden_test
+
+import (
+	"errors"
+	"reflect"
+	"testing"
+	"time"
+
+	"github.com/Warashi/dataloaden"
+)
+
+func TestLoader_Load(t *testing.T) {
+	fetch := func(keys []int) ([]int, []error) {
+		ret := make([]int, len(keys))
+		retErr := make([]error, len(keys))
+		for i := range keys {
+			if keys[i]%2 == 0 {
+				ret[i] = keys[i] * 10
+			} else {
+				retErr[i] = errors.New("some error")
+			}
+		}
+		return ret, retErr
+	}
+	config := dataloaden.LoaderConfig[int, int]{
+		Fetch: fetch,
+		Wait:  1 * time.Millisecond,
+	}
+	loader := dataloaden.NewLoader(config)
+
+	tests := []struct {
+		name    string
+		arg     int
+		want    int
+		wantErr bool
+	}{
+		{name: "non-error/0", arg: 0, want: 0, wantErr: false},
+		{name: "error/1", arg: 1, want: 0, wantErr: true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := loader.Load(tt.arg)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Load() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("Load() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestLoader_LoadAll(t *testing.T) {
+	fetch := func(keys []int) ([]int, []error) {
+		ret := make([]int, len(keys))
+		retErr := make([]error, len(keys))
+		for i := range keys {
+			if keys[i]%2 == 0 {
+				ret[i] = keys[i] * 10
+			} else {
+				retErr[i] = errors.New("some error")
+			}
+		}
+		return ret, retErr
+	}
+	config := dataloaden.LoaderConfig[int, int]{
+		Fetch: fetch,
+		Wait:  1 * time.Millisecond,
+	}
+	loader := dataloaden.NewLoader(config)
+
+	tests := []struct {
+		name    string
+		arg     []int
+		want    []int
+		wantErr []bool
+	}{
+		{name: "normal", arg: []int{0, 1, 2, 3}, want: []int{0, 0, 20, 0}, wantErr: []bool{false, true, false, true}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := loader.LoadAll(tt.arg)
+			gotErr := make([]bool, len(err))
+			for i := range err {
+				gotErr[i] = err[i] != nil
+			}
+			if !reflect.DeepEqual(gotErr, tt.wantErr) {
+				t.Errorf("Load() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Load() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
